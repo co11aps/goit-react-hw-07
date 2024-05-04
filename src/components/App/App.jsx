38a34a8058a25bccff8ae4c8a81229e212eead95
co11aps <143988142+co11aps@users.noreply.github.com>
2024-05-04 23:48:29 +0800

    hw7

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,12 +2,16 @@ import ContactList from "../ContactList/ContactList";
 import SearchBox from "../SearchBox/SearchBox";
 import ContactForm from "../ContactForm/ContactForm";
 import css from "./App.module.css";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { fetchContacts } from "../../redux/contactsOps";
+import { selectError, selectIsLoading } from "../../redux/selectors";
 
 const App = () => {
   const dispatch = useDispatch();
+
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
 
   useEffect(() => {
     dispatch(fetchContacts());
@@ -18,6 +22,8 @@ const App = () => {
       <h1>Phonebook</h1>
       <ContactForm />
       <SearchBox />
+      {isLoading && <p>Loading...</p>}
+      {error && <p>Something went wrong! Try to reload the page!</p>}
       <ContactList />
     </div>
   );
